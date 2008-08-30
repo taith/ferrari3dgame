b7ba8af0347556c02ca7a4d9f6870e65b7922c33
mail <mail@dennisbijlsma.com>
2008-08-30 09:23:32 +0000

    Initial commit of tests.

---
+++ source.test/com/dennisbijlsma/ferrari3d/test/TestMultiplayer.java
@@ -0,0 +1,16 @@
+package com.dennisbijlsma.ferrari3d.test;
+
+import com.dennisbijlsma.ferrari3d.*;
+import com.dennisbijlsma.ferrari3d.util.*;
+
+
+
+public class TestMultiplayer {
+
+	
+	
+	public static void main(String[] args) {
+	
+		//TODO
+	}
+}
