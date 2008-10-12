0c99afc8130890680122813a21a495e4923eef02
mail <mail@dennisbijlsma.com>
2008-10-12 19:29:35 +0000

    Updating source files.

---
+++ source/com/dennisbijlsma/ferrari3d/Multiplayer.java
@@ -0,0 +1,337 @@
+//--------------------------------------------------------------------------------
+// Ferrari3D
+// Multiplayer
+//--------------------------------------------------------------------------------
+
+package com.dennisbijlsma.ferrari3d;
+
+import java.util.*;
+
+import com.dennisbijlsma.core3d.data.*;
+import com.dennisbijlsma.ferrari3d.util.*;
+import com.dennisbijlsma.xmlserver.*;
+
+/**
+ * Controls the multiplayer connections and allows for sending of messages. This
+ * class uses <code>XML Server</code> initially. This allows all XML-based 
+ * messages to be sent over a number of protocols.<br><br>
+ * Sending should be done by calling one of the methods in this class, which will
+ * convert the parameters to an XML messages and send it. Receiving messages can
+ * however be done by any class implementing the <code>MPReceiver</code> interface.
+ * These can be registered to listen to all messages for a specific ID, or to 
+ * receive all generic messages.
+ */
+
+public class Multiplayer implements ConnectionListener {
+	
+	private XMLServer server;
+	private ConnectionManager manager;
+	private MessageQueue receiveQueue;
+	
+	public static final String MESSAGE_INIT="init";
+	public static final String MESSAGE_DISCONNECT="disconnect";
+	public static final String MESSAGE_CONNECT="connect";
+	public static final String MESSAGE_UPDATE="update";
+	public static final String MESSAGE_START="start";
+	public static final String MESSAGE_STOP="stop";
+	public static final String MESSAGE_START_SESSION="startSession";
+	public static final String MESSAGE_STOP_SESSION="stopSession";
+	public static final String MESSAGE_PAUSE="pause";
+	public static final String MESSAGE_CHAT="chat";
+	
+	private static final Multiplayer INSTANCE=new Multiplayer();
+		
+	/**
+	 * Private constructor as this class should not be initialized.
+	 */
+	
+	private Multiplayer() {
+		
+		super();
+		
+		manager=new SocketManager(Settings.getInstance().MULTIPLAYER_PORT);
+		manager.addConnectionListener(this);
+		server=new XMLServer(manager);
+		
+		receiveQueue=new MessageQueue();		
+	}
+	
+	/**
+	 * Returns the only existing instance of this class.
+	 */
+	
+	public static Multiplayer getInstance() {
+	
+		return INSTANCE;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	
+	public void connected(Connection connection) {
+		
+		Message message=new XMLMessage();
+		message.setType(MESSAGE_INIT);
+		message.setParameter("id",connection.getMeta("id"));
+		
+		for (ConnectionListener i : manager.getConnectionListeners()) {
+			i.messageReceived(connection,message);
+		}
+		
+		print("Connected: "+connection.getHost());
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	
+	public void disconnected(Connection connection) { 
+		
+		Message message=new XMLMessage();
+		message.setType(MESSAGE_DISCONNECT);
+		message.setParameter("id",connection.getMeta("id"));
+		
+		for (ConnectionListener i : manager.getConnectionListeners()) {
+			i.messageReceived(connection,message);
+		}
+		
+		print("Disconnected: "+connection.getHost());
+	}
+	
+	/**
+	 * {@inheritDoc} 
+	 */
+	
+	public void messageReceived(Connection connection,Message message) {
+		
+		receiveQueue.addMessage(message);
+		print("<<< "+message);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */	
+	
+	public void messageSent(Connection connection,Message message) { 
+		
+		print(">>> "+message);
+	}
+	
+	/**
+	 * Sends the specified message. This method is the core of sending all 
+	 * multiplayer messages. Numerous convenience methods are available in this
+	 * class for creating and sending certain types of messages directly.
+	 */
+	
+	private void sendMessage(Message message) {
+	
+		if (message instanceof XMLMessage) {
+			((XMLMessage) message).setUseHeader(false);
+		}
+		
+		manager.sendAll(message);
+	}
+	
+	/**
+	 * Sends a message with position and lap data for the specified contestant.
+	 * This message should be sent every frame. When the game is running as a
+	 * server, it should also send update data for all AI cars.
+	 */	
+	
+	public void sendUpdateMessage(Contestant contestant) {
+		
+		Vector3D position=contestant.getPosition();
+		
+		Message message=new XMLMessage();
+		message.setType(MESSAGE_UPDATE);
+		message.setParameter("id",contestant.getID());
+		message.setParameter("position",position.x+","+position.y+","+position.z);
+		message.setParameter("orientation",""+contestant.getOrientation());
+		message.setParameter("direction",""+contestant.getDirection());
+		message.setParameter("lap",""+contestant.getLap());
+		message.setParameter("intermediate",""+contestant.getIntermediate());
+		message.setParameter("point",""+contestant.getPoint());
+		message.setParameter("laptime",""+contestant.getCurrentLaptime().getCurrentTime());
+		message.setParameter("speed",""+contestant.getSpeed(false));
+		message.setParameter("angular",""+contestant.getAngular());
+		sendMessage(message);
+	}
+	
+	/**
+	 * Sends a message that the game has been started. This should be sent by the
+	 * server when the menu is terminated and loading of the game is beginning. 
+	 */
+	
+	public void sendStartMessage() {
+		
+		Message message=new XMLMessage();
+		message.setType(MESSAGE_START);
+		sendMessage(message);
+	}
+	
+	/**
+	 * Sends a message that the game has been stopped. This should be sent by the
+	 * server when the game is terminated and the menu is recreated.
+	 */
+	
+	public void sendStopMessage() {
+	
+		Message message=new XMLMessage();
+		message.setType(MESSAGE_STOP);
+		sendMessage(message);
+	}
+	
+	/**
+	 * Sends a message that the session is started. This method should be sent by
+	 * the server, either directly after loading or when the green lights have been
+	 * shown (in race mode).
+	 */
+	
+	public void sendStartSessionMessage() {
+	
+		Message message=new XMLMessage();
+		message.setType(MESSAGE_START_SESSION);
+		sendMessage(message);
+	}
+	
+	/**
+	 * Sends a message that the session is stopped. This method can be sent by 
+	 * both client and server, when they signal that one of the contestants has
+	 * met the requirements needed to finish the session.
+	 */
+	
+	public void sendStopSessionMessage() {
+		
+		Message message=new XMLMessage();
+		message.setType(MESSAGE_STOP_SESSION);
+		sendMessage(message);
+	}
+	
+	/**
+	 * Sends a message that the paused state has changed. This can be sent by both
+	 * the client and server, when the game is paused or depaused by one of them.
+	 */
+	
+	public void sendPauseMessage(boolean state) {
+	
+		Message message=new XMLMessage();
+		message.setType(MESSAGE_PAUSE);
+		message.setParameter("state",""+state);
+		sendMessage(message);
+	}
+	
+	/**
+	 * Sends a chat message. This method can be sent both in the menu and in-game.
+	 * The sender field for the message can be manually specified.
+	 */
+	
+	public void sendChatMessage(String from,String chatMessage) {
+	
+		Message message=new XMLMessage();
+		message.setType(MESSAGE_STOP);
+		message.setParameter("from",from);
+		message.setParameter("message",chatMessage);
+		sendMessage(message);
+	}
+	
+	/**
+	 * Flushes all received messages since the last time this method was called.
+	 * The messages will be returned to whoever called the method.<br><br>
+	 * To prevent synchronization problems, this method must be called only
+	 * during the game loop.
+	 */
+	
+	public synchronized Message[] flushReceivedMessages() {
+		
+		return receiveQueue.flushAll();
+	}
+	
+	/**
+	 * Creates a new server connection with this class.
+	 */
+	
+	public void connectAsServer() {
+		
+		server.connectServer();
+	}
+	
+	/**
+	 * Creates a new client connection with this class.
+	 */
+	
+	public boolean connectAsClient() {
+		
+		return server.connectClient(Settings.getInstance().server);
+	}
+	
+	/**
+	 * Disconnects all connections. When this class is running as a server, it 
+	 * will end the current session.
+	 */
+	
+	public void disconnect() {
+	
+		server.disconnect();
+	}
+	
+	/**
+	 * Resets all variables to their original state. This method should be called
+	 * between sessions.
+	 */
+	
+	public void reset() {
+	
+		receiveQueue.clear();
+	}
+	
+	/**
+	 * Returns true if this class is currently connected. This method is a 
+	 * shorthand for <code>XMLServer.isConnected()</code>.
+	 */
+	
+	public boolean isConnected() {
+	
+		return server.isConnected();
+	}
+	
+	/**
+	 * Returns true if this class is currently running as a server. This method
+	 * is a shorthand for <code>XMLServer.isServer()</code>. 
+	 */
+	
+	public boolean isServer() {
+		
+		return server.isServer();
+	}
+	
+	/**
+	 * Returns the <code>XMLServer</code> that is used by this class.
+	 */
+	
+	public XMLServer getXMLServer() {
+		
+		return server;
+	}
+	
+	/**
+	 * Returns the <code>ConnectionManager</code> that is used by this class.
+	 */
+	
+	public ConnectionManager getConnectionManager() {
+	
+		return manager;
+	}
+	
+	/**
+	 * Method for controlling debug output. This output could be written to
+	 * the console, to the log file, or not at all.
+	 */
+	
+	private void print(String message) {
+	
+		if (Settings.getInstance().debug) {
+			System.out.println("Multiplayer  "+System.currentTimeMillis()+"   "+message);
+		}
+	}
+}
---
+++ source/com/dennisbijlsma/ferrari3d/graphics/SAT.java
@@ -0,0 +1,312 @@
+//--------------------------------------------------------------------------------
+// Ferrari3D
+// SAT
+//--------------------------------------------------------------------------------
+
+package com.dennisbijlsma.ferrari3d.graphics;
+
+import java.awt.*;
+import java.awt.image.*;
+import javax.swing.*;
+
+import com.dennisbijlsma.ferrari3d.util.*;
+import com.dennisbijlsma.util.data.*;
+import com.dennisbijlsma.util.swing.*;
+
+
+
+public class SAT {
+
+	private Graphics2D g2;
+	private Rectangle area;
+	private int timer;
+		
+	private static final Font FONT=Utils.loadFont("data/fonts/manana.ttf",Font.PLAIN,15f);
+	private static final Font BIG_FONT=Utils.loadFont("data/fonts/manana.ttf",Font.PLAIN,40f);
+	private static final Color FONT_COLOR_MAIN=new Color(255,255,255);
+	private static final Color FONT_COLOR_SUB=new Color(0,200,255);
+	private static final Color FONT_COLOR_TIME=new Color(255,200,0);
+	private static final Color FONT_COLOR_INFO=new Color(0,200,255);
+	private static final Color FONT_COLOR_DIFF=new Color(255,220,0);
+	private static final Color FONT_COLOR_POS=new Color(0,0,0);
+	private static final Color FONT_COLOR_POS_1=new Color(0,0,0);
+	private static final Color BACKGROUND_COLOR=new Color(0,0,0,128);
+	private static final Color MAIN_PANEL_COLOR=null;
+	private static final Color SUB_PANEL_COLOR=null;
+	private static final Color CENTER_PANEL_COLOR=null;
+	private static final Color DIFF_COLOR_PLUS=null;
+	private static final Color DIFF_COLOR_MIN=null;
+	private static final Color POS_COLOR=new Color(255,220,0);
+	private static final Color POS_COLOR_1=new Color(255,220,0);
+	private static final int MAIN_PANEL_WIDTH=150;
+	private static final int SUB_PANEL_WIDTH=120;
+	private static final int CENTER_PANEL_WIDTH=100;
+	private static final int STANDINGS_NAME_WIDTH=120;
+	private static final int STANDINGS_TIME_WIDTH=60;
+	private static final int PANEL_HEIGHT=25;
+	private static final int SPACING=10;
+	private static final int PADDING=5;
+	
+	
+	
+	public SAT() {
+
+		g2=null;
+		area=new Rectangle(0,0,1,1);
+	}
+	
+	
+	
+	public void paintDefaultScreen(String driver,String car,int pos) {
+				
+		driver=nameFormat(driver.toUpperCase());
+		car=car.toUpperCase();
+		
+		paintBackground();
+		paintPanel(driver,FONT,MAIN_PANEL_COLOR,FONT_COLOR_MAIN,'l',1,MAIN_PANEL_WIDTH,1,'l');
+		paintPanel(car,FONT,SUB_PANEL_COLOR,FONT_COLOR_SUB,'l',2,MAIN_PANEL_WIDTH,1,'l');
+		
+		if (pos>0) {
+			paintPanel(""+pos,BIG_FONT,POS_COLOR,FONT_COLOR_POS,'r',1,PANEL_HEIGHT*2,2,'c');
+		}
+	}
+	
+	
+	
+	public void paintDefaultScreen(String driver,String car) {
+	
+		paintDefaultScreen(driver,car,0);
+	}
+	
+	
+	
+	public void paintTimeScreen(String driver,Laptime laptime) {
+		
+		driver=nameFormat(driver.toUpperCase());
+		String time=Utils.timeFormat(laptime.getCurrentTime());
+		time=time.substring(0,time.length()-2);
+
+		paintBackground();
+		paintPanel(driver,FONT,MAIN_PANEL_COLOR,FONT_COLOR_MAIN,'l',1,MAIN_PANEL_WIDTH,1,'l');
+		paintPanel(time,FONT,SUB_PANEL_COLOR,FONT_COLOR_TIME,'l',2,SUB_PANEL_WIDTH,1,'r');
+	}
+	
+	
+	
+	public void paintTimeDiffScreen(String driver1,Laptime laptime1,String driver2,
+			Laptime laptime2,int intermediate,boolean diff,int pos) {
+		
+		driver1=nameFormat(driver1.toUpperCase());
+		driver2=nameFormat(driver2.toUpperCase());
+		
+		String time1=Utils.timeFormat(laptime1.getIntermediateTime(intermediate));
+		String time2=Utils.timeFormat(laptime2.getIntermediateTime(intermediate));
+		String timeDiff=Utils.timeDiffFormat(laptime1.getIntermediateTime(intermediate),laptime2.getIntermediateTime(intermediate));
+		String intText=getIntermediateLabel(intermediate);
+		
+		if (!diff) {
+			time1=time1.substring(0,time1.length()-2);
+		}
+		
+		paintBackground();
+		paintPanel(driver1,FONT,MAIN_PANEL_COLOR,FONT_COLOR_MAIN,'l',1,MAIN_PANEL_WIDTH,1,'l');
+		paintPanel(time1,FONT,SUB_PANEL_COLOR,FONT_COLOR_TIME,'l',2,SUB_PANEL_WIDTH,1,'r');
+		paintPanel(intText,FONT,CENTER_PANEL_COLOR,FONT_COLOR_INFO,'c',4,CENTER_PANEL_WIDTH,1,'c');
+
+		if (intermediate!=2) {
+			paintPanel(driver2,FONT,MAIN_PANEL_COLOR,FONT_COLOR_MAIN,'r',1,MAIN_PANEL_WIDTH,1,'r');
+			paintPanel(time2,FONT,SUB_PANEL_COLOR,FONT_COLOR_TIME,'r',2,SUB_PANEL_WIDTH,1,'l');
+		} else {
+			paintPosRect(pos,area.width/2+CENTER_PANEL_WIDTH/2,SPACING,true);
+		}			
+		
+		if (diff) {
+			paintPanel(timeDiff,FONT,DIFF_COLOR_PLUS,FONT_COLOR_DIFF,'c',3,CENTER_PANEL_WIDTH,1,'c');
+		}
+	}
+	
+	
+	
+	public void paintPosDiffScreen(String driver1,int pos1,String driver2,int pos2,int diff) {
+			
+		driver1=nameFormat(driver1.toUpperCase());
+		driver2=nameFormat(driver2.toUpperCase());
+		String timeDiff=Utils.timeDiffFormat(diff,0);
+		int posX1=area.width/2-CENTER_PANEL_WIDTH/2-MAIN_PANEL_WIDTH-PANEL_HEIGHT*2;
+		int posX2=area.width/2+CENTER_PANEL_WIDTH/2+MAIN_PANEL_WIDTH;
+		
+		paintBackground();
+		paintPanel(driver1,FONT,MAIN_PANEL_COLOR,FONT_COLOR_MAIN,'l',1,MAIN_PANEL_WIDTH,1,'l');
+		paintPanel(driver2,FONT,MAIN_PANEL_COLOR,FONT_COLOR_MAIN,'r',1,MAIN_PANEL_WIDTH,1,'r');
+		paintPosRect(pos1,posX1,SPACING,true);
+		paintPosRect(pos2,posX2,SPACING,true);
+
+		if (diff>0) {
+			paintPanel(timeDiff,FONT,DIFF_COLOR_PLUS,FONT_COLOR_DIFF,'c',1,CENTER_PANEL_WIDTH,1,'c');
+		}
+
+	}
+	
+	
+	
+	public void paintStandingsScreen(String[] drivers,Laptime[] laptimes) {
+	
+		paintBackground();
+		
+		for (int i=0; i<Math.min(drivers.length,8); i++) {
+			String driver=nameFormat(drivers[i].toUpperCase());
+			String time=Utils.timeDiffFormat(laptimes[i],laptimes[0]);
+			if (i==0) { time=Utils.timeFormat(laptimes[i]); }
+			
+			int x1=area.width/2-STANDINGS_NAME_WIDTH-STANDINGS_TIME_WIDTH-PANEL_HEIGHT;
+			int x2=x1+PANEL_HEIGHT;
+			int x3=x2+STANDINGS_NAME_WIDTH;
+			int y=SPACING+i*PANEL_HEIGHT;
+			
+			if (i>3) {
+				x1+=STANDINGS_NAME_WIDTH+STANDINGS_TIME_WIDTH+PANEL_HEIGHT;
+				x2+=STANDINGS_NAME_WIDTH+STANDINGS_TIME_WIDTH+PANEL_HEIGHT;
+				x3+=STANDINGS_NAME_WIDTH+STANDINGS_TIME_WIDTH+PANEL_HEIGHT;
+				y-=PANEL_HEIGHT*4;
+			}
+			
+			paintPosRect(i+1,x1,y,false);
+			paintTextRect(driver,FONT,MAIN_PANEL_COLOR,FONT_COLOR_MAIN,x2,y,STANDINGS_NAME_WIDTH,PANEL_HEIGHT,'l',PADDING,PADDING);
+			paintTextRect(time,FONT,SUB_PANEL_COLOR,FONT_COLOR_TIME,x3,y,STANDINGS_TIME_WIDTH,PANEL_HEIGHT,'r',PADDING,PADDING);
+		}
+	}
+	
+	/**
+	 * Paints the background panel. If the area to paint is the same size as the 
+	 * last time, the background is painted from an image.
+	 */
+	
+	private void paintBackground() {
+				
+		if (BACKGROUND_COLOR==null) {
+			return;
+		}
+		
+		g2.setColor(BACKGROUND_COLOR);
+		g2.fillRect(area.x,area.y,area.width,area.height);
+	}
+	
+	
+	
+	private void paintRect(Color color,int x,int y,int width,int height) {
+		
+		if (color!=null) {
+			g2.setColor(color);
+			g2.fillRect(x,y,width,height);
+		}
+	}
+	
+	
+	
+	private void paintText(String text,Font font,Color color,int x,int y,char align) {
+	
+		g2.setFont(font);
+		g2.setColor(color);
+		Utils2D.drawAlignedString(g2,text,x,y,align);
+	}
+	
+	
+	
+	private void paintTextRect(String text,Font font,Color bgColor,Color textColor,int x,int y,
+			int width,int height,char align,int padX,int padY) {
+		
+		paintRect(bgColor,x,y,width,height);
+		
+		if (align=='l') { paintText(text,font,textColor,x+padX,y+height-padY,'l'); }
+		if (align=='c') { paintText(text,font,textColor,x+width/2,y+height-padY,'c'); }
+		if (align=='r') { paintText(text,font,textColor,x+width-padX,y+height-padY,'r'); }
+	}
+	
+	
+	
+	private void paintPanel(String text,Font font,Color bgColor,Color textColor,char column,
+			int row,int width,int lines,char align) {
+		
+		int x=area.x+area.width/2;
+		int y=SPACING+(row-1)*PANEL_HEIGHT;
+		
+		if (column=='l') { x-=CENTER_PANEL_WIDTH/2+width; }
+		if (column=='c') { x-=CENTER_PANEL_WIDTH/2; }
+		if (column=='r') { x+=CENTER_PANEL_WIDTH/2; }
+		
+		paintTextRect(text,font,bgColor,textColor,x,y,width,PANEL_HEIGHT*lines,align,PADDING,PADDING*lines);
+	}
+	
+	
+	
+	private void paintPosRect(int pos,int x,int y,boolean large) {
+		
+		Font font=(!large) ? FONT : BIG_FONT;
+		int size=(!large) ? PANEL_HEIGHT : PANEL_HEIGHT*2;
+		Color bgColor=(pos==1) ? POS_COLOR_1 : POS_COLOR;
+		Color textColor=(pos==1) ? FONT_COLOR_POS : FONT_COLOR_POS_1;
+		
+		paintTextRect(""+pos,font,bgColor,textColor,x+1,y+1,size-2,size-2,'c',PADDING,large ? PADDING*2 : PADDING);
+	}
+	
+	
+	
+	private String getIntermediateLabel(int intermediate) {
+	
+		switch (intermediate) {
+			case 0 : return "INTERMEDIATE 1"; 
+			case 1 : return "INTERMEDIATE 2";
+			case 2 : return "START / FINISH";
+			default : return "???";
+		}
+	}
+	
+	
+	
+	public void setGraphics(Graphics2D g2) {
+	
+		this.g2=g2;
+	}
+	
+	
+	
+	public Graphics2D getGraphics() {
+	
+		return g2;
+	}
+	
+	
+	
+	public void setArea(int x,int y,int width,int height) {
+	
+		area.setBounds(x,y,width,height);
+	}
+	
+	
+	
+	public Rectangle getArea() {
+	
+		return area;
+	}
+	
+	
+	
+	public void setTimer(int timer) {
+	
+		this.timer=Math.max(timer,0);
+	}
+	
+	
+	
+	public int getTimer() {
+	
+		return timer;
+	}
+	
+	
+	
+	private String nameFormat(String name) {
+	
+		return TextFormatter.nameFormat(name,false);
+	}
+}
